685236b9e3334db3864fa7f1c92051e9ae7cfec9
Nathael Arki <nath.arki36@gmail.com>
2019-11-11 03:13:59 +0100

    linter on newly created strategy

--- src/commands/cd/strategies/pathExists.strategy.js
+++ src/commands/cd/strategies/pathExists.strategy.js
@@ -4,36 +4,36 @@ const { getDir } = require('../../../filesystem/getDir')
 const { dirExists } = require('../../../filesystem')
 
 const test = req => {
-    const {
-        username,
-        env: { PWD: pwd }
-      } = req.session
-    
-    const [dir = `~`] = getArgs(req.body.line)
-    const fullPath = getDir({ username, pwd, dir }).replace(/\\/g, '/')
+  const {
+    username,
+    env: { PWD: pwd }
+  } = req.session
 
-    return dirExists({
-        dir: fullPath,
-        username,
-        session: req.session
-      })
+  const [dir = `~`] = getArgs(req.body.line)
+  const fullPath = getDir({ username, pwd, dir }).replace(/\\/g, '/')
+
+  return dirExists({
+    dir: fullPath,
+    username,
+    session: req.session
+  })
 }
 
 const exec = req => {
-    const {
-      username,
-      env: { PWD: pwd }
-    } = req.session
-    const [dir = `~`] = getArgs(req.body.line)
-    const fullPath = getDir({ username, pwd, dir }).replace(/\\/g, '/')
+  const {
+    username,
+    env: { PWD: pwd }
+  } = req.session
+  const [dir = `~`] = getArgs(req.body.line)
+  const fullPath = getDir({ username, pwd, dir }).replace(/\\/g, '/')
 
-    sessions.updateWhere(session => 
-        session.id === req.body.id,
-        data => {
-            data.env.PWD = fullPath
-        }
-    )
-    return []
+  sessions.updateWhere(
+    session => session.id === req.body.id,
+    data => {
+      data.env.PWD = fullPath
+    }
+  )
+  return []
 }
 
 module.exports = {
