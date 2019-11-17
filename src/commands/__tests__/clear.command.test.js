8ae130913c109ef3b44376b4187e3f4b5089d3f4
Joel Thoms <jthoms@gmail.com>
2019-11-16 17:21:30 -0800

    refactor: 💡 add lint:fix

--- src/commands/__tests__/clear.command.test.js
+++ src/commands/__tests__/clear.command.test.js
@@ -4,6 +4,8 @@ const actions = require('../../actions')
 describe('commands/clear', () => {
   test('execution of the command returns an array', () => {
     const actual = exec()
-    expect(actual).toStrictEqual([actions.echo('\u001b[2J\u001b[0;0H', { crlf: false })])
+    expect(actual).toStrictEqual([
+      actions.echo('\u001b[2J\u001b[0;0H', { crlf: false })
+    ])
   })
 })
