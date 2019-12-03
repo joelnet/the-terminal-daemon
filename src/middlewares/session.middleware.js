87fb6892aeced85c9483c9dfe955b8c744a4ee70
cagomezr <carlos.a.gomez.romero@gmail.com>
2019-12-03 18:36:57 -0500

    Update session.middleware.js
    Merging session-id.middleware.js to
    session .middleware.js
    
     adding chalk , config and actions to   requiremenrs
    
     adding variable is post
    
    we then  using router.use we add code from session-id  in lines 13 to 16 and in line 17 we add a  17  else condition. on the code from
    
    Merging complete thanks for letting me contribute to your project.
    
    Im not deleting your session-id as I believe the code is needed elsewhere and   I would need more knowledge to do this.
    
    have a great day

--- src/middlewares/session.middleware.js
+++ src/middlewares/session.middleware.js
@@ -1,12 +1,20 @@
+const chalk = require('chalk')
+const config = require('config')
 const express = require('express')
 const router = express.Router()
 const { sessions } = require('../stores/memory')
+const actions = require('../actions')
 
+const isPost = req => req.method === 'POST'
 const hasSessionId = req =>
   req.body.id != null && sessions.find({ id: { $eq: req.body.id } }).length > 0
 
 router.use((req, res, next) => {
-  if (hasSessionId(req)) {
+  if (isPost(req) && !hasSessionId(req)) {
+    const message = config.get('copy.invalid-session')
+    res.json([actions.echo(chalk.red(message)), actions.exit()])
+    next(message)
+  }else if (hasSessionId(req)) {
     req.session = sessions.find({ id: { $eq: req.body.id } })[0]
   }
 
